4ad45319c5da01c614f269a9d92bce15b7499771
BartekP <bartekp050@gmail.com>
2026-03-20 11:35:50 +0100

    czołg "działa"

---
+++ player.gd
@@ -0,0 +1,28 @@
+extends VehicleBody3D
+
+const ENGINE_POWER = 71960.0
+@onready var left_wheels = [$VehicleWheel3D,$VehicleWheel3D2,
+$VehicleWheel3D3,$VehicleWheel3D4,$VehicleWheel3D5]
+@onready var right_wheels = [$VehicleWheel3D6,$VehicleWheel3D7,
+$VehicleWheel3D8,$VehicleWheel3D9,$VehicleWheel3D10]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var steer = Input.get_axis("right","left")
+	var move_dir = Input.get_axis("backward","forward")
+	var engine_power = 20.0
+	if steer != 0:
+		for wheel in left_wheels:
+			wheel.engine_force = ENGINE_POWER * -steer * 2
+		for wheel in right_wheels:
+			wheel.engine_force = ENGINE_POWER * steer * 2
+	else:
+		for wheel in left_wheels:
+			wheel.engine_force = ENGINE_POWER * move_dir
+		for wheel in right_wheels:
+			wheel.engine_force = ENGINE_POWER * move_dir
+		
