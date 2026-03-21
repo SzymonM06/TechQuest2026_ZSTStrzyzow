6989c0d0ea8358b5f55f4bdd63c16c073c89d0bc
BartekP <bartekp050@gmail.com>
2026-03-21 09:02:48 +0100

    win screen

--- level.gd
+++ level.gd
@@ -4,12 +4,16 @@ var max_targets = 10
 # Called when the node enters the scene tree for the first time.
 var points = 0
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label2.visible = true
+	await get_tree().create_timer(3).timeout
+	$Label2.visible = false
 
 func _add_point():
 	points = points + 1
+	$Label.text = "Cele " + str(points) + "/10"
 	print(points)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(points == max_targets):
 		print("win")
+		get_tree().change_scene_to_file("res://win_screen.tscn")
---
+++ win_screen.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://menu.tscn")
