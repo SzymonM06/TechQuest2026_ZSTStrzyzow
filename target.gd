b1aef9ad99fdf330eeeaa33ccb3bde069213901a
BartekP <bartekp050@gmail.com>
2026-03-21 06:44:53 +0100

    Target done

---
+++ target.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("bullet"):
+		$AnimationPlayer.play("falling")
