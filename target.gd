1f6be84d07723a282c85f63731f63158fec1c5f7
BartekP <bartekp050@gmail.com>
2026-03-21 07:53:45 +0100

    trying

--- target.gd
+++ target.gd
@@ -15,4 +15,5 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("bullet") and !onfloor:
 		$AnimationPlayer.play("falling")
+		$Area3D.visible = false
 		onfloor = true
